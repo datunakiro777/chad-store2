8d2185322089af4243db70cc2f30fc54811043be
datunakiro777 <115496613+datunakiro777@users.noreply.github.com>
2025-02-23 17:46:55 +0400

    Delete chad-store directory

--- chad-store/products/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from products.models import Product, ProductTag, Review
-
-admin.site.register(Product)
-admin.site.register(ProductTag)
-admin.site.register(Review)
--- chad-store/products/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from products.views import product_view
-
-urlpatterns = [
-    path('products/', product_view, name="products")
-]
