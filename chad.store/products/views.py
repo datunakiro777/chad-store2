d138cdf70c214e593ddc70ed1b0422171c93ce7c
unknown <davitpapidze777@gmail.com>
2025-02-23 21:21:33 +0400

    api views learned further

--- chad.store/products/views.py
+++ chad.store/products/views.py
@@ -3,51 +3,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin
-from products.models import (
-    Product,
-    Review,
-    FavoriteProduct,
-    Cart, ProductTag, ProductImage
-)
-from products.serializers import (
-    ProductSerializer,
-    ReviewSerializer,
-    FavoriteProductSerializer,
-    CartSerializer,
-    ProductTagSerializer, ProductImageSerializer
-    )
+from products.models import Product, Review, FavoriteProduct, Cart, ProductTag, ProductImage
+from products.serializers import ProductSerializer, ReviewSerializer, FavoriteProductSerializer, CartSerializer, ProductTagSerializer, ProductImageSerializer
 
 
-class ProductViewSet(RetrieveModelMixin, 
-                    CreateModelMixin,
-                    ListModelMixin,
-                    UpdateModelMixin,
-                    DestroyModelMixin,
-                    GenericAPIView):
 
+class ProductViewSet(ListCreateAPIView, RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-
-    def get(self, request, pk=None, *args, **kwargs):
-        if pk:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
     
     
     
